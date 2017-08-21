0b4be25822acda1c433ec0561f8a2804352c1f98
M100122\student <enjoy767@gmail.com>
2017-08-21 16:46:46 +0900

    두번째 버전

--- src/lab1/Test3.java
+++ src/lab1/Test3.java
@@ -1,7 +1,7 @@
 package lab1;
 
-public class Test2 {
+public class Test3 {
 	public static void main(String[] args) {
-		System.out.println("test222222222 ");
+		System.out.println("Test3 class");
 	}
 }
